3cfc2aa4747d02d286bbff578ebc171d5c04b719
Daniel Bonn <danieltbonn@gmail.com>
2023-07-17 20:43:45 -0500

    trying to add the weather api to scripts.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,24 @@
 
 function getApi(requestUrl) {
+    console.log(requestUrl);
 
-    fetch (requestUrl) 
+    var resolve = fetch (requestUrl , {}) 
     .then(function (response) {
-        return response.json();
+        console.log(response);
+        var newResponse = response.json();
+        console.log(newResponse);
+        return newResponse;
     })
     .then(function (data) {
         console.log(data);
-    })
+    });
+
+    return resolve;
 }
 
-var requestUrl = "";
+var requestUrl = 
+"api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=2f8d2f4d2713c98d73c6bee0108a1778";
+
+requestUrl = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=2f8d2f4d2713c98d73c6bee0108a1778";
+var finished = getApi(requestUrl);
+console.log(finished);
