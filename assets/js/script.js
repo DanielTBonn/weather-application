c50ac7d90ca8eccbad4d0d5d8a8bb8cab887ba45
Daniel Bonn <danieltbonn@gmail.com>
2023-07-17 21:46:11 -0500

    added jquery and working on adding weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,8 +16,16 @@ function getApi(requestUrl) {
     return resolve;
 }
 
+var todayEl = $(".todays-weather");
+console.log(todayEl);
+$.each(todayEl.children(), function(item) { 
+    console.log(item);
+});
+
+
 var requestUrl = 
 "api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=2f8d2f4d2713c98d73c6bee0108a1778";
 
 requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=2f8d2f4d2713c98d73c6bee0108a1778";
+
 getApi(requestUrl);
