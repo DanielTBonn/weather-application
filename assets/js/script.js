3cfd7d78d4e21078f231812c812bf674d45a8405
Daniel Bonn <danieltbonn@gmail.com>
2023-07-17 21:24:03 -0500

    code to recieve weather info

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 function getApi(requestUrl) {
     console.log(requestUrl);
 
-    var resolve = fetch (requestUrl , {}) 
+    fetch (requestUrl , {}) 
     .then(function (response) {
         console.log(response);
         var newResponse = response.json();
@@ -13,12 +13,10 @@ function getApi(requestUrl) {
         console.log(data);
     });
 
-    return resolve;
 }
 
 var requestUrl = 
 "api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=2f8d2f4d2713c98d73c6bee0108a1778";
 
-requestUrl = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=2f8d2f4d2713c98d73c6bee0108a1778";
-var finished = getApi(requestUrl);
-console.log(finished);
+requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=2f8d2f4d2713c98d73c6bee0108a1778";
+getApi(requestUrl);
