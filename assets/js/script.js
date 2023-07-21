ea4d55ab736e4f30e107daec96f1574112a9c0df
Daniel Bonn <danieltbonn@gmail.com>
2023-07-20 22:33:41 -0500

    added functionality to insert fontawesome icons into html elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,21 @@ var weatherCards = $("[id^=weather-card]");
 console.log(weatherCards);
 console.log(weatherCards.length)
 console.log(today);
+var ele = document.createElement("i")
+ele.setAttribute("class", "fa-solid fa-snowflake")
+console.log(ele)
+$(".todays-weather").append(ele)
+
 // Adding a weathermap to grab weather emojis depending on climate conditions
 const weatherMap = new Map([
-    ["apples", 500]
+    ["snow", '<i class="fa-solid fa-snowflake"></i>']
 ]);
 
 // This function grabs the openweathermap API data and returns info that will be extracted
 function getApi(requestUrl) {
     console.log(requestUrl);
 
+    // fetch grabs our api
     var resolve = fetch (requestUrl , {}) 
     .then(function (response) {
         console.log(response);
@@ -20,10 +26,11 @@ function getApi(requestUrl) {
         console.log(newResponse);
         return newResponse;
     })
+    // info is taken from data and fills out the cards of the page
     .then(function (data) {
         console.log(data)
         console.log(data.weather)
-        resultData = ["Date: " + today ,"Climate: " + data.list[0].weather[0].main, "Temp: " + data.list[0].main.temp, "Wind: " + data.list[0].wind.speed, "Humidity: " + data.list[0].main.humidity];
+        resultData = ["Date: " + today ,"Climate:  " + data.list[0].weather[0].main, "Temp: " + data.list[0].main.temp, "Wind: " + data.list[0].wind.speed, "Humidity: " + data.list[0].main.humidity];
         setData(resultData, ".todays-weather");
 
         for (i = 0; i < weatherCards.length; i++) {
@@ -55,8 +62,14 @@ function setData(arr, eVal) {
     var currentEl = $(eVal);
     console.log(currentEl)
     $.each(currentEl.children("p"), function(index) { 
-        console.log(index);
-        $(this).text(arr[index]);
+        if (index === 1) {
+            console.log($(this))
+            $(this).append(weatherMap.get("snow"))
+        } else {
+            console.log(index);
+            $(this).text(arr[index]);
+
+        }
     });
 }
 
